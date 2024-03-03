ea36dfedc94b429914f5d0aca82792899c6e1436
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-04 01:40:14 +0530

    Create a custom USER model

---
+++ accounts/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+class UserManager(BaseUserManager):
+    # create user
+    def create_user(self, first_name, last_name, user_name, email, password):
+        # validations
+        if not first_name:
+            raise ValueError('Users must have a first name')
+        if not last_name:
+            raise ValueError('Users must have a last name')
+        if not user_name:
+            raise ValueError('Users must have a username')
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not password:
+            raise ValueError('Users must have a password')
+        
+        # create user
+        user = self.model(
+            email = self.normalize_email(email),
+            user_name = user_name,
+            first_name = first_name,
+            last_name = last_name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    # create superuser
+    def create_superuser(self, first_name, last_name, user_name, email, password):
+        # create user
+        user = self.create_user(
+            email = self.normalize_email(email),
+            user_name = user_name,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        # set user as admin
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+    
+
+
+class User(AbstractBaseUser):
+    RESTURANT = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICE = (
+        (RESTURANT, 'Resturant'),
+        (CUSTOMER, 'Customer')
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True, unique=True)
+    password = models.CharField(max_length=100)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+
+    # Django required fields
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name', 'first_name', 'last_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
--- eat_online_main/settings.py
+++ eat_online_main/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
