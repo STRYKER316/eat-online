1944a48ac966d1ce872df9ed0cf525a8a4c615c7
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-04 15:19:51 +0530

    New user creation from form data using create_user() from UserManager

--- accounts/views.py
+++ accounts/views.py
@@ -5,17 +5,31 @@ from accounts.models import User
 
 # Create your views here.
 def register_user(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.role = User.CUSTOMER
-            user.save()
-            return redirect('register_user')
-
+    # GET request
     if request.method == 'GET':
         form = UserForm()
         context = {
             'form': form
         }
         return render(request, 'accounts/register_user.html', context)
+    
+    # POST request
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # User creation using create_user() from UserManager
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            user_name = form.cleaned_data.get('user_name')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                user_name=user_name,
+                email=email,
+                password=password
+            )
+            user.role = User.CUSTOMER
+
+            return redirect('register_user')
