d88180d111a8dcba493dfa61647fd3d08936c42c
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-04 12:49:56 +0530

    Add User Account registration path

--- eat_online_main/urls.py
+++ eat_online_main/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    # admin site
     path('admin/', admin.site.urls),
+    # home page
     path('', views.home, name='home'),
+    # customer registration
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
